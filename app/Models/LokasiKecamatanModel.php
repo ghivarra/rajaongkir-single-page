9e23513598e17aa3fbd71038a0e5574b8bf67081
WIN 10 Desktop GVR <gsenandika@gmail.com>
2023-01-26 14:15:18 +0700

    update orm lokasi

--- app/Models/LokasiKecamatanModel.php
+++ app/Models/LokasiKecamatanModel.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class LokasiKecamatanModel extends Model
 {
     use HasFactory;
+
+    protected $table = 'lokasi_kecamatan';
+    protected $primaryKey = 'id';
+    protected $incrementing = TRUE;
+    
+    protected $fillable = ['id', 'nama', 'lokasi_kota_id', 'lokasi_provinsi_id'];
 }
--- app/Models/LokasiKotaModel.php
+++ app/Models/LokasiKotaModel.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class LokasiKotaModel extends Model
 {
     use HasFactory;
+
+    protected $table = 'lokasi_kota';
+    protected $primaryKey = 'id';
+    protected $incrementing = TRUE;
+    
+    protected $fillable = ['id', 'nama', 'lokasi_provinsi_id'];
 }
