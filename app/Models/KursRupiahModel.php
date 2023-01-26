24541809c0e6b03eee313c63409258960b685eff
WIN 10 Desktop GVR <gsenandika@gmail.com>
2023-01-26 14:13:44 +0700

    update orm kurs rupisah

--- app/Models/KursRupiahModel.php
+++ app/Models/KursRupiahModel.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class KursRupiahModel extends Model
 {
     use HasFactory;
+
+    protected $table = 'kurs_rupiah';
+    protected $primaryKey = 'id';
+    protected $incrementing = TRUE;
+    
+    protected $fillable = ['id', 'mata_uang', 'mata_uang_pendek', 'value', 'sumber_nama', 'sumber_link', 'last_update'];
 }
