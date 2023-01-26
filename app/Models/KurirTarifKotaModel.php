be6973e0a698f07e8c7953a46b19a7129b780056
WIN 10 Desktop GVR <gsenandika@gmail.com>
2023-01-26 14:11:23 +0700

    update orm kurir tarif kota

--- app/Models/KurirTarifKotaModel.php
+++ app/Models/KurirTarifKotaModel.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class KurirTarifKotaModel extends Model
 {
     use HasFactory;
+
+    protected $table = 'kurir_tarif_kota';
+    protected $primaryKey = 'id';
+    
+    protected $fillable = ['id', 'kota_asal_id', 'kota_tujuan_id', 'last_update', 'kurir_id', 'berat', 'dimensi_panjang', 'dimensi_lebar', 'dimensi_tinggi', 'dimensi_diameter', 'rincian_biaya'];
 }
