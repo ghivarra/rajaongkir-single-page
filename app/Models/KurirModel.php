76ca475abeffdb82e6c3a7ee789be07a8676f416
WIN 10 Desktop GVR <gsenandika@gmail.com>
2023-01-26 14:05:48 +0700

    init commit

---
+++ app/Models/KurirModel.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class KurirModel extends Model
+{
+    use HasFactory;
+
+    protected $table = 'permohonan_lsp';
+    protected $primaryKey = 'id';
+    protected $keyType = 'string';
+
+    public $incrementing = TRUE;
+    public $timestamps = TRUE;
+
+    protected $guarded = [];
+    protected $fillable = ['id', 'nama', 'nama_pendek', 'logo', 'is_aktif', 'is_cek_ongkir', 'is_cek_resi', 'is_lokal', 'is_internasional', 'warna'];
+
+    protected $dispatchesEvents = [];
+}
