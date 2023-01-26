15f7c56074c1a4c96b8f271f658e3df45aa68912
WIN 10 Desktop GVR <gsenandika@gmail.com>
2023-01-26 14:08:25 +0700

    update orm kurir

--- app/Models/KurirModel.php
+++ app/Models/KurirModel.php
@@ -9,12 +9,9 @@ class KurirModel extends Model
 {
     use HasFactory;
 
-    protected $table = 'permohonan_lsp';
+    protected $table = 'kurir';
     protected $primaryKey = 'id';
     protected $keyType = 'string';
-
-    protected $guarded = [];
+    
     protected $fillable = ['id', 'nama', 'nama_pendek', 'logo', 'is_aktif', 'is_cek_ongkir', 'is_cek_resi', 'is_lokal', 'is_internasional', 'warna'];
-
-    protected $dispatchesEvents = [];
 }
