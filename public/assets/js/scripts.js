dfd23769558bcfc145959e1f887e816f74124973
WIN 10 Desktop GVR <gsenandika@gmail.com>
2023-01-30 15:59:07 +0700

    git done tampilan lacak paket

--- public/assets/js/scripts.js
+++ public/assets/js/scripts.js
@@ -7,18 +7,186 @@ const cekResiForm = document.getElementById('pills-lacak-paket');
 cekResiForm.addEventListener('submit', function(e) {
 	e.preventDefault();
 
+	document.getElementById('cr-result').classList.add('d-none');
+
+	Swal.fire({
+		text: 'Memproses Permohonan',
+		showConfirmButton: false,
+		showCloseButton: false,
+		allowOutsideClick: false,
+		didOpen: function() {
+			Swal.showLoading();
+		}
+	});
+
 	// ajax post
 	xhrPOST({
 		link: url('lacak-paket'),
 		data: new FormData(cekResiForm),
 		catch: function() {
-			Swal.fire('Permohonan Gagal Diproses', 'Server sedang mengalami gangguan, silahkan coba dalam beberapa menit', 'warning');
+			Swal.hideLoading();
+			Swal.update({
+				title: 'Permohonan Gagal Diproses',
+				text: 'Server sedang mengalami gangguan, silahkan coba lagi dalam beberapa menit',
+				icon: 'warning',
+				allowOutsideClick: true,
+				showConfirmButton: true,
+			});
 		},
 		error: function() {
-			Swal.fire('Permohonan Gagal Diproses', 'Gagal menghubungi server, periksa koneksi internet anda', 'error');
+			Swal.hideLoading();
+			Swal.update({
+				title: 'Permohonan Gagal Diproses',
+				text: 'Gagal menghubungi server, periksa koneksi internet anda',
+				icon: 'warning',
+				allowOutsideClick: true,
+				showConfirmButton: true,
+			});
 		},
 		success: function(res) {
-			console.log(res);
+			res = JSON.parse(res);
+
+			if (res.code !== 200) {
+				Swal.hideLoading();
+				Swal.update({
+					title: 'Permohonan Gagal Diproses',
+					text: res.description,
+					icon: 'error',
+					allowOutsideClick: true,
+					showConfirmButton: true,
+				});
+
+				return;
+			}
+
+			// mutasi data
+			let courier = res.courier;
+			let result  = res.result;
+			let summary = result.summary;
+
+			// set data kurir
+			let crKurirLogo = document.getElementById('cr-kurir-logo');
+			crKurirLogo.setAttribute('src', url('assets/images/kurir/' + courier.logo));
+			crKurirLogo.setAttribute('alt', summary.courier_name);
+
+			// set warna title
+			document.getElementById('cr-title-1').style.color = courier.warna;
+			document.getElementById('cr-title-2').style.color = courier.warna;
+			document.getElementById('cr-title-3').style.color = courier.warna;
+
+			// set status pengiriman
+			let crStatus = document.getElementById('cr-status');
+			let statusPengiriman = (result.delivered === true) ? 'TERKIRIM' : 'DIKIRIM';
+			let statusColor = (result.delivered === true) ? 'text-success' : 'text-warning';
+
+			crStatus.innerText = statusPengiriman;
+			crStatus.classList.remove('text-success', 'text-warning');
+			crStatus.classList.add(statusColor);
+
+			// set detail pengiriman
+			document.getElementById('cr-detail-resi').innerText = result.details.waybill_number;
+			document.getElementById('cr-detail-datetime').innerText = result.details.datetime;
+			document.getElementById('cr-detail-shipper').innerText = summary.shipper_name;
+			document.getElementById('cr-detail-shipper-city').innerText = result.details.origin;
+			document.getElementById('cr-detail-receiver').innerText = summary.receiver_name;
+			document.getElementById('cr-detail-receiver-city').innerText = result.details.destination;
+
+			// set histori pengiriman
+			let crHistoryTimeline = document.getElementById('cr-history-timeline');
+			crHistoryTimeline.innerHTML = '';
+
+			Array.prototype.forEach.call(result.history, function (item, i) {
+				let section = document.createElement('section');
+				let dateDiv = document.createElement('div');
+				let innerDateDiv = document.createElement('div');
+				let dateText = document.createElement('p');
+				let dateDayText = document.createElement('p');
+
+				// set date & time text
+				dateDayText.innerText = item.day;
+				dateDayText.classList.add('mb-0');
+
+				dateText.innerText = item.date;
+				dateText.classList.add('mb-0');
+
+				innerDateDiv.appendChild(dateDayText);
+				innerDateDiv.appendChild(dateText);
+				innerDateDiv.classList.add('bg-dark', 'text-white', 'py-2', 'px-3', 'rounded');
+
+				dateDiv.appendChild(innerDateDiv);
+				dateDiv.classList.add('border-start', 'border-2', 'border-secondary', 'd-flex', 'px-4', 'pb-4', 'd-md-none');
+
+				// set history left & right
+				let outerDiv = document.createElement('div');
+				let historyRight = document.createElement('div');
+				let historyLeft = document.createElement('div');
+				let hlDate = document.createElement('p');
+				let hlDay = document.createElement('p');
+
+				hlDay.innerText = item.day;
+				hlDay.classList.add('mb-1', 'fw-bold');
+
+				hlDate.innerText = item.date;
+				hlDate.classList.add('mb-0');
+
+				historyLeft.appendChild(hlDay);
+				historyLeft.appendChild(hlDate);
+				historyLeft.classList.add('cr-history-left', 'd-none', 'd-md-block', 'pe-4');
+
+				historyRight.classList.add('cr-history-right');
+
+				outerDiv.appendChild(historyLeft);
+				outerDiv.appendChild(historyRight);
+				outerDiv.classList.add('d-flex');
+
+				section.appendChild(dateDiv);
+				section.appendChild(outerDiv);
+				section.classList.add('cr-history-date', 'cr-history-' + i);
+
+				// foreach manifests
+				Array.prototype.forEach.call(item.manifests, function (manifest, n) {
+					let manifestDiv = document.createElement('div');
+					let manifestCircle = document.createElement('div');
+					let manifestInner = document.createElement('div');
+					let manifestInnerDesc = document.createElement('p');
+					let manifestInnerTime = document.createElement('p');
+
+					manifestInnerTime.innerText = manifest.time;
+					manifestInnerTime.classList.add('mb-0', 'fw-bold');
+
+					manifestInnerDesc.innerText = manifest.desc;
+					manifestInnerDesc.classList.add('mb-1');
+
+					manifestInner.appendChild(manifestInnerTime);
+					manifestInner.appendChild(manifestInnerDesc);
+					manifestInner.classList.add('cr-manifest-details');
+
+					manifestCircle.classList.add('cr-history-right-circle', 'rounded-circle');
+					manifestCircle.style.backgroundColor = courier.warna;
+
+					manifestDiv.appendChild(manifestCircle);
+					manifestDiv.appendChild(manifestInner);
+					manifestDiv.classList.add('position-relative', 'px-4', 'pb-5', 'border-start', 'border-2', 'border-secondary');
+
+					historyRight.appendChild(manifestDiv);
+				});
+
+				crHistoryTimeline.appendChild(section);
+			});
+
+			// display none
+			document.getElementById('cr-result').classList.remove('d-none');
+
+			Swal.close();
+
+			setTimeout(function() {
+				let pos = document.getElementById('cr-result').getBoundingClientRect();
+				window.scrollTo({
+					top: pos.top,
+					left: pos.left,
+					behavior: 'smooth'
+				});
+			}, 300);
 		}
 	});
 });
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,72 +1,130 @@
 @include('header')
 
 <main class="main text-dark d-flex align-items-center justify-content-center py-5">
+
+	<div class="w-100">
 	
-	<section class="main-section bg-white rounded shadow-sm px-4 py-3">
-		
-		<div class="mb-4">
-			<h5 class="fw-bold mb-3">Pilih Layanan</h5>
-			<ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
-			  	<li class="nav-item border border-secondary border-2" role="presentation">
-			    	<button class="nav-link rounded-0 active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true">
-			    		Cek Ongkir
-			    	</button>
-			  	</li>
-			  	<li class="nav-item border border-secondary border-2" role="presentation">
-			    	<button class="nav-link rounded-0" id="pills-lacak-paket-tab" data-bs-toggle="pill" data-bs-target="#pills-lacak-paket" type="button" role="tab" aria-controls="pills-lacak-paket" aria-selected="false">
-			    		Lacak Paket
-			    	</button>
-			  	</li>
-			</ul>
-		</div>
+		<section class="main-section bg-white rounded shadow-sm px-4 py-3 mx-auto">
+			
+			<div class="mb-4">
+				<h5 class="fw-bold mb-3">Pilih Layanan</h5>
+				<ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+				  	<li class="nav-item border border-secondary border-2" role="presentation">
+				    	<button class="nav-link rounded-0 active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true">
+				    		Cek Ongkir
+				    	</button>
+				  	</li>
+				  	<li class="nav-item border border-secondary border-2" role="presentation">
+				    	<button class="nav-link rounded-0" id="pills-lacak-paket-tab" data-bs-toggle="pill" data-bs-target="#pills-lacak-paket" type="button" role="tab" aria-controls="pills-lacak-paket" aria-selected="false">
+				    		Lacak Paket
+				    	</button>
+				  	</li>
+				</ul>
+			</div>
 
-		<div class="tab-content">
-  			<div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab" tabindex="0">...</div>
-  			<form action="{{ url('lacak-paket') }}" method="POST" class="tab-pane fade" id="pills-lacak-paket" role="tabpanel" aria-labelledby="pills-lacak-paket-tab" tabindex="0">
+			<div class="tab-content">
+	  			<div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab" tabindex="0">...</div>
+	  			<form action="{{ url('lacak-paket') }}" method="POST" class="tab-pane fade" id="pills-lacak-paket" role="tabpanel" aria-labelledby="pills-lacak-paket-tab" tabindex="0">
 
-  				@csrf
+	  				@csrf
 
-  				<div class="alert alert-warning mb-3" role="alert">
-  					API Rajaongkir belum menyediakan fitur pengecekan resi/pelacakan paket untuk jasa ekspedisi 
+	  				<div class="alert alert-warning mb-3" role="alert">
+	  					API Rajaongkir belum menyediakan fitur pengecekan resi/pelacakan paket untuk jasa ekspedisi 
 
-  					@foreach ($cekResiKurirNotAvailable as $n => $item)
+	  					@foreach ($cekResiKurirNotAvailable as $n => $item)
 
-  						@if (isset($cekResiKurirNotAvailable[($n + 1)]))
-  							<b>{{ $item['nama_pendek'] }}</b>,
-  						@else
-  							dan <b>{{ $item['nama_pendek'] }}</b>
-  						@endif
+	  						@if (isset($cekResiKurirNotAvailable[($n + 1)]))
+	  							<b>{{ $item['nama_pendek'] }}</b>,
+	  						@else
+	  							dan <b>{{ $item['nama_pendek'] }}</b>
+	  						@endif
 
-  					@endforeach
+	  					@endforeach
 
-  				</div>
+	  				</div>
 
-  				<div class="form-group mb-3">
-  					<label for="cr-kurir" class="form-label mb-2 fw-bold">Jasa Ekspedisi/Kurir</label>
-  					<select id="cr-kurir" class="form-select" name="courier" required>
-  						<option value="">Pilih Jasa Ekspedisi</option>
+	  				<div class="form-group mb-3">
+	  					<label for="cr-kurir" class="form-label mb-2 fw-bold">Jasa Ekspedisi/Kurir</label>
+	  					<select id="cr-kurir" class="form-select" name="courier" required>
+	  						<option value="">Pilih Jasa Ekspedisi</option>
 
-  						@foreach ($cekResiKurir as $item)
+	  						@foreach ($cekResiKurir as $item)
 
-  							<option value="{{ $item['id'] }}">{{ $item['nama_pendek'] }} - {{ $item['nama'] }}</option>
+	  							<option value="{{ $item['id'] }}">{{ $item['nama_pendek'] }} - {{ $item['nama'] }}</option>
 
-  						@endforeach
+	  						@endforeach
 
-  					</select>
-  				</div>
+	  					</select>
+	  				</div>
 
-  				<div class="form-group mb-4">
-  					<label for="cr-resi" class="form-label mb-2 fw-bold">Nomor Resi</label>
-  					<input id="cr-resi" type="text" class="form-control" name="waybill" placeholder="Input resi pengiriman anda..." required>
-  				</div>
-  				<div class="form-action">
-  					<button type="submit" class="btn btn-primary text-light me-2">Lacak</button>
-  					<button type="reset" class="btn btn-outline-primary">Kosongkan</button>
-  				</div>
-  			</form>
-		</div>
+	  				<div class="form-group mb-4">
+	  					<label for="cr-resi" class="form-label mb-2 fw-bold">Nomor Resi</label>
+	  					<input id="cr-resi" type="text" class="form-control" name="waybill" placeholder="Input resi pengiriman anda..." required>
+	  				</div>
+	  				<div class="form-action">
+	  					<button type="submit" class="btn btn-primary text-light me-2">Lacak</button>
+	  					<button type="reset" class="btn btn-outline-primary">Kosongkan</button>
+	  				</div>
+	  			</form>
+			</div>
 
-	</section>
+		</section>
+
+		<section id="cr-result" class="mt-5 d-none bg-white p-4 mx-auto">
+
+			<div class="mb-5">
+				<figure class="m-0">
+					<img id="cr-kurir-logo">
+				</figure>
+			</div>
+
+			<div class="mb-5">
+				<h5 id="cr-title-1" class="fw-bold mb-4">Status Pengiriman</h5>
+				<p id="cr-status" class="fw-bold fs-5"></p>
+			</div>
+			
+			<div class="mb-5">
+				
+				<div class="mb-5">
+					<h5 id="cr-title-2" class="fw-bold mb-4">Detail Pengiriman</h5>
+					<div class="row mb-4 mb-lg-0 row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+						<div class="col">
+							<h6 class="fw-bold">No. Resi</h6>
+							<p id="cr-detail-resi"></p>
+						</div>
+						<div class="col">
+							<h6 class="fw-bold">Waktu Pengiriman</h6>
+							<p id="cr-detail-datetime"></p>
+						</div>
+						<div class="col">
+							<h6 class="fw-bold">Kota/Kode Pos Asal</h6>
+							<p id="cr-detail-shipper-city"></p>
+						</div>
+						<div class="col">
+							<h6 class="fw-bold">Kota/Kode Pos Tujuan</h6>
+							<p id="cr-detail-receiver-city"></p>
+						</div>	
+						<div class="col">
+							<h6 class="fw-bold">Pengirim</h6>
+							<p id="cr-detail-shipper"></p>
+						</div>
+						<div class="col">
+							<h6 class="fw-bold">Penerima</h6>
+							<p id="cr-detail-receiver"></p>
+						</div>			
+					</div>
+				</div>
+
+				<div>
+					<h5 id="cr-title-3" class="fw-bold mb-4">Histori Pengiriman</h5>
+					<div id="cr-history-timeline"></div>
+				</div>
+
+			</div>
+
+		</section>
+
+	</div>
 
 </main>
 
