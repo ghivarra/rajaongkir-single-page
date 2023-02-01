885e076dfca69d3759bc1944e736a41eba9794cb
WIN 10 Desktop GVR <gsenandika@gmail.com>
2023-02-01 13:52:57 +0700

    add orm internasional tujuan

---
+++ app/Models/InternasionalTujuanModel.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class InternasionalTujuanModel extends Model
+{
+    use HasFactory;
+
+    public $timestamps = FALSE;
+
+    protected $table = 'internasional_tujuan';
+    protected $primaryKey = 'id';    
+    protected $fillable = ['id', 'nama'];
+}
